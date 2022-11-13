1f4a477dd9b1162957222ef01c3da70a1f0d6dd9
Morgan Batterham <morgan@morganbatterham.me>
2022-11-13 20:28:37 +1100

    upgraded to createRoot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import Game from './Components/Game'
 
 
-
-
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Game />);
