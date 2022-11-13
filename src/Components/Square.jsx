5f56006901bdf0c2e1b27ceb2dfc94f5783c8ba8
Morgan Batterham <morgan@morganbatterham.me>
2022-11-13 19:56:35 +1100

    converted components to functions

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button
-          className="square" 
-          onClick ={() => this.props.onClick()}
-          >
-          {this.props.value}
-        </button>
-      );
-    }
+const square = ({ onClick, value }) =>  {
+
+    return (
+      <button
+        className="square" 
+        onClick ={() => onClick()}
+        >
+        {value}
+      </button>
+    );
+
   }
 
   export default Square
